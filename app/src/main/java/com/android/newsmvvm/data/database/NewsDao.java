4a14d169d16ceb42ceb2b3203ec491b058bcb773
animesh288 <animesh.satish@gmail.com>
2022-03-21 02:55:17 +0530

    added dao

---
+++ app/src/main/java/com/android/newsmvvm/data/database/NewsDao.java
@@ -0,0 +1,22 @@
+package com.android.newsmvvm.data.database;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.android.newsmvvm.data.model.Article;
+
+import java.util.List;
+
+@Dao
+public interface NewsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addArticle(Article article);
+
+    @Query("SELECT * from Article")
+    List<Article> getArticles();
+}
